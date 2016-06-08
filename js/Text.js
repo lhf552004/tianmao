5152dd6a14f5236151b62e18bb2f0917e64ed3e6
Allen Li <Allen Li>
2016-06-08 01:55:12 -0700

    ignore content

--- js/Text.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Operator on 5/16/2016.
- */
-function Text(id, label, OriginalText, innerText) {
-    this.id = id;
-    this.label = label;
-    this.OriginalText = OriginalText;
-    this.innerText =  innerText;
-}
